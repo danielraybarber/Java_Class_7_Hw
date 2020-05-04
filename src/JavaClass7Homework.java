4c88ff84529a16c07df18082bf529826759fd549
Daniel Barber <Daniel@Daniels-MacBook-Pro.local>
2020-05-03 23:14:48 -0500

    Java Homework 7

---
+++ src/JavaClass7Homework.java
@@ -0,0 +1,16 @@
+
+public class JavaClass7Homework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Question 1 answer is C. 20
+		
+		//Question 2
+		for(int x = 0; x < 5; x++) {
+			for(int y = 0; y<=x; y++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}		
+	}
+}
